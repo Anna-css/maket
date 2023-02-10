7c2ee55e9ebdd52596be61c9893c53a8eb96d1a0
Anna-css <124602700+Anna-css@users.noreply.github.com>
2023-02-10 12:05:15 +0300

    added

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+let loginButton = document.querySelector( '.login-button');
+let loginPopUp = document.querySelector('.login-popup');
+
+loginButton.addEventListener('click',showPopUp);
+
+function showPopUp (evt) {    
+    evt.preventDefault ();
+    loginPopUp.classList.toggle('show-popup');
+};
